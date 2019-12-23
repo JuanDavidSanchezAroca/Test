f244556edaac114226a09ba92eaba97de546db37
JuanDavidSanchezAroca <juan.david748@gmail.com>
2019-12-23 08:46:01 -0500

    [JDSANCHEZ] proyecto inicial

---
+++ ProductStore/src/test/java/co/productstore/ProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.productstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
